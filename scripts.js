f4a3455d001b67126947f80f4629837c37b71715
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-13 17:53:26 +0100

    Fixed HTML and JS

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 const menuButtons = document.querySelectorAll('.side-menu-button');
 const sections = document.querySelectorAll('.section');
-const midmenu = document.querySelector('.mid-menu-container')
+const midMenus = document.querySelectorAll('.mid-menu-container')
 
 menuButtons.forEach((button, index) => {
     button.addEventListener('click', (e) => {
         // Remove the active class from all buttons and sections
         menuButtons.forEach((button) => button.classList.remove('active'));
         sections.forEach((section) => section.classList.remove('active'));
-        midmenu.forEach((midmenu) => section.classList.remove('active'));
+        midMenus.forEach((midMenu) => midMenu.classList.remove('active'));
 
         // Set the clicked button and corresponding section to active
         menuButtons[index].classList.add('active');
         sections[index].classList.add('active');
-        midmenu[index].classlist.add('active')
+        midMenus[index].classList.add('active');
     });
 });
