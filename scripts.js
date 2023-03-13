8bec734c98ae67d6ee711ddcba1cacee2f24b1b4
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-13 17:28:58 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
 const menuButtons = document.querySelectorAll('.side-menu-button');
 const sections = document.querySelectorAll('.section');
-
-// Set the first menu button and section to active by default
-menuButtons[0].classList.add('active');
-sections[0].classList.add('active');
+const midmenu = document.querySelector('.mid-menu-container')
 
 menuButtons.forEach((button, index) => {
     button.addEventListener('click', (e) => {
         // Remove the active class from all buttons and sections
         menuButtons.forEach((button) => button.classList.remove('active'));
         sections.forEach((section) => section.classList.remove('active'));
+        midmenu.forEach((midmenu) => section.classList.remove('active'));
 
         // Set the clicked button and corresponding section to active
-        button.classList.add('active');
+        menuButtons[index].classList.add('active');
         sections[index].classList.add('active');
+        midmenu[index].classlist.add('active')
     });
 });
