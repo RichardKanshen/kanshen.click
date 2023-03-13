6664da97e75caef54f77049c3c23a55bc3786b1d
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-13 18:41:57 +0100

    Added colorétte article

--- scripts.js
+++ scripts.js
@@ -1,17 +1,31 @@
-const menuButtons = document.querySelectorAll('.side-menu-button');
+const sideMenuButtons = document.querySelectorAll('.side-menu-button');
+const midMenuButtons = document.querySelectorAll('.mid-menu-button');
 const sections = document.querySelectorAll('.section');
 const midMenus = document.querySelectorAll('.mid-menu-container')
+const articles = document.querySelectorAll('article');
 
-menuButtons.forEach((button, index) => {
+sideMenuButtons.forEach((button, index) => {
     button.addEventListener('click', (e) => {
         // Remove the active class from all buttons and sections
-        menuButtons.forEach((button) => button.classList.remove('active'));
+        sideMenuButtons.forEach((button) => button.classList.remove('active'));
         sections.forEach((section) => section.classList.remove('active'));
         midMenus.forEach((midMenu) => midMenu.classList.remove('active'));
 
         // Set the clicked button and corresponding section to active
-        menuButtons[index].classList.add('active');
+        sideMenuButtons[index].classList.add('active');
         sections[index].classList.add('active');
         midMenus[index].classList.add('active');
     });
 });
+
+midMenuButtons.forEach((button, index) => {
+    button.addEventListener('click', (e) => {
+        // Remove the active class from all articles
+        articles.forEach((article) => article.classList.remove('active'));
+
+        // Set the corresponding article to active
+        articles[index].classList.add('active');
+    });
+});
+
+
